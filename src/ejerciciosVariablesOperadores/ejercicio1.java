accb6341134b3d2e33ee11854a2d89cd9e3452b6
jferrer299 <joseph.ferrer@educa.madrid.org>
2021-10-07 17:17:08 +0200

    Se agrega la modificacion del ejercicio 1 de enteros

--- src/ejerciciosVariablesOperadores/ejercicio1.java
+++ src/ejerciciosVariablesOperadores/ejercicio1.java
@@ -6,7 +6,9 @@ public class ejercicio1 {
                 int n1 = 50, n2 = 30;
                 int suma = n1 + n2 ;
                 System.out.println("La suma es: " + suma);
-
+//  Esta es la modificacion para la práctica nº4 de Entornos de desarrollo (Git + GitHub)
+                int producto = n1 * n2;
+                System.out.println("la multiplicacion es : " + producto);
 
     }
 }
